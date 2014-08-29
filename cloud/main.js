5d5202ee28ca5ed91c350bf5d116ffd69f7074c1
ChenFeicun <276549366@qq.com>
2014-08-29 11:37:18 +0800

    test

--- cloud/main.js
+++ cloud/main.js
@@ -19,45 +19,4 @@ AV.Cloud.define("getUserByObjId", function(request, response) {
 	});
 });
 
-//根据objectId来查找对应的用户
-AV.Cloud.define("getWPUser", function(request, response) {
-	var query = new AV.Query("WPUser");
-	query.equalTo("objectId", request.params.objectId);
-	query.find({
-		success: function(results) {
-			var point = new AV.GeoPoint({latitude: result[0].UserLatitude, longitude: result[0].UserLongitude});
-			result[0].set("UserLocation", point);
-			result[0].save();
-			response.success(results[0]);
-		},
-		error: function() {
-			response.error("User not found");
-		}
-	});
-});
-
-//计算两个GPS点之间的距离
-AV.Cloud.define("getDistance" function(request, response) {
-	var user;
-	AV.Cloud.run('getUserByObjId', {}, {
-		success: function(result) {
-			user = result;
-		},
-		error: function(error) {
-		}
-	});//getUserByObjId(request, response);
-	var point = user.UserLocation;
-	var query = new AV.Query("WPUser");
-	query.notEqualTo("objectId", user.objectId);
-	query.withinKilometers("UserLocation", point, 0.5);
-	query.find({
-		success: function(results) {
-			response.success(results);
-		},
-		error: function() {
-			response.error("User not found");
-		}
-	});
-});
-
 
